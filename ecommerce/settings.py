8e43092c7197472531f2a0c67907d7b32d076435
vishal <gajjarvishal288@gmail.com>
2026-06-29 17:20:07 +0530

    Added PostgreSQL support

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-import dj_database_url
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,25 +83,16 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 
 # Database configuration
-DATABASE_URL = os.environ.get('DATABASE_URL')
 
-if DATABASE_URL:
-    import dj_database_url  # <-- Hum safe side rehne ke liye yahan bhi import likh dete hain
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
+import os
+import dj_database_url
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
