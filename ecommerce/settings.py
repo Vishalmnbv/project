414db9602b2822f92e6a3c8a8aa439bde0f66067
vishal <gajjarvishal288@gmail.com>
2026-06-29 12:11:47 +0530

    added postgresql configuration with ssl

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,10 +90,9 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'postgres://project1_db_5w3s_user:JX3rWBTeCdjZisLqcSXXxiaZYI18rvBP@dpg-d90vu8u8bjmc739iqq3g-a.oregon-postgres.render.com/project1_db_5w3s?sslmode=require')
+    )
 }
 
 
