31110c7ace1a89e035b8f84810d2c42e937293bf
vishal <gajjarvishal288@gmail.com>
2026-06-30 14:06:20 +0530

    Initial commit for render

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-ngitc!%2%ttpn7p6rm+-j&&6px-*9%2v3guo!dtww7tf5f*sq4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
