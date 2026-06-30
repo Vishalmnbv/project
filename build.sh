fd060438d6a74470344dabff4ae24fc959625c41
vishal <gajjarvishal288@gmail.com>
2026-06-30 15:27:31 +0530

    added loaddata command to build script

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+# exit on error
+set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+# Collect Static Files
+python manage.py collectstatic --no-input
 
+# Run Migrations
 python manage.py migrate
 
+# YE WALI LINE SABSE NEECHE ADD KAREIN:
+python manage.py loaddata db_backup.json
