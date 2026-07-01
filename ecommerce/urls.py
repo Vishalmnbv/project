87dc157f36434d1c94edc35dbdd6c80e0d1ad7c8
vishal <gajjarvishal288@gmail.com>
2026-07-01 10:15:53 +0530

    Remove static re_path

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -29,10 +29,11 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 else:
-    # Render (Live Server) માટે આ સૌથી સેફ રીત છે:
     urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+        re_path(
+            r'^media/(?P<path>.*)$',
+            serve,
+            {'document_root': settings.MEDIA_ROOT},
+        ),
     ]
